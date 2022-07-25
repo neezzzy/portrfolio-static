e536c326544d9309c24caf9816be825401a05b91
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2022-07-24 20:54:12 -0700

    add chatter app project

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,2 @@
 var date = new Date().getFullYear();
 document.getElementById('year').innerHTML = date;
-
-$(document).ready(function loopToggle() {
-  $('.spark').animate({ fontSize: '15' }, 2000);
-  $('.spark').animate({ fontSize: '25' }, 2000, loopToggle);
-});
